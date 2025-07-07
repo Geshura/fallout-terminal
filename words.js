d44129a5078f1467a4103aefc8d9d99ebe336817
Geshura <geshura@gmail.com>
2025-07-08 00:11:33 +0200

    2.0

---
+++ words.js
@@ -0,0 +1,77 @@
+const words4 = [
+  "KOTE",
+  "DOMY",
+  "PLOT",
+  "ROZA",
+  "KAWA",
+  "MIEL",
+  "LASY",
+  "OGON",
+  "GRAD",
+  "MOST"
+];
+
+const words5 = [
+  "KOTKA",
+  "DOMKI",
+  "PLOTY",
+  "ROZAN",
+  "KAWKA",
+  "MIELI",
+  "LASKA",
+  "OGONY",
+  "GRADY",
+  "MOSTY"
+];
+
+const words6 = [
+  "KOTKOW",
+  "DOMKOW",
+  "PLOTKA",
+  "ROZANA",
+  "KAWYKA",
+  "MIELIC",
+  "LASKIY",
+  "OGONKI",
+  "GRADZI",
+  "MOSTEK"
+];
+
+const words7 = [
+  "KOTKOWY",
+  "DOMKOWA",
+  "PLOTKAM",
+  "ROZANIE",
+  "KAWKAMI",
+  "MIELICY",
+  "LASKIEM",
+  "OGONKOW",
+  "GRADZIK",
+  "MOSTEKI"
+];
+
+const words8 = [
+  "KOTKOWYM",
+  "DOMKOWAŁ",
+  "PLOTKAMI",
+  "ROZANIEK",
+  "KAWKAMIĘ",
+  "MIELICZA",
+  "LASKIEMI",
+  "OGONKOWA",
+  "GRADZIKI",
+  "MOSTEKIN"
+];
+
+const words9 = [
+  "KOTKOWYMI",
+  "DOMKOWAŁY",
+  "PLOTKAMIA",
+  "ROZANIEKU",
+  "KAWKAMIEM",
+  "MIELICZAN",
+  "LASKIEMIO",
+  "OGONKOWAN",
+  "GRADZIKOM",
+  "MOSTEKINO"
+];
